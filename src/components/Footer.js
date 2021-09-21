e1283e89a4522676bf5617341da155d85620aa6c
Justin Green <justingreenmedia@gmail.com>
2021-09-21 12:39:08 -0500

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
 	return (
 		<footer className="footer">
-			&copy; 2021 <a href="https://www.justingreen.dev/" target="_blank" rel="noreferrer">Justin Green</a>
+			Built by <a href="https://www.justingreen.dev/" target="_blank" rel="noreferrer">Justin Green</a>
 		</footer>
 	)
 }
